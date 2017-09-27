da4a56207258aea7589e7e3215ca5cf8a988825d
Mark Ludovice <markjoseph.ludovice@bicol-u.edu.ph>
2017-09-27 09:06:33 +0800

    Final without pagination pa

--- main/html/userendpages/.event-recent.php
+++ main/html/userendpages/.event-recent.php
@@ -3,6 +3,28 @@
 	include '../../../vendor/Parsedown/Parsedown.php';
 	require_once '../../../admin/php/backend/input_handler.php';
 ?>
+
+<style>
+.pic img {
+  height: 150px;
+  width:150px;
+  border-radius:50%;
+
+ 
+  -webkit-transition: all 1s ease;
+     -moz-transition: all 1s ease;
+       -o-transition: all 1s ease;
+      -ms-transition: all 1s ease;
+          transition: all 1s ease;
+
+}
+ 
+.pic img:hover {
+  width: 200px;
+  height: 200px;
+  
+</style>
+
 <link rel="stylesheet" type="text/css" href="../css/breadcrumb.css">
 <script type="text/javascript" src="userendpages/loadjs/viewS.js"></script>
 <div id="content2">												<!--  T  H  E     C  O  N  T  E  N  T  -->
@@ -26,10 +48,10 @@
 							$row['imgbanner'] = "../../data/events-stories/noimage.jpg";
 						}
 						echo '
-						<div onclick="viewStory('.$row['post_id'].')">
+						<div class = "pic" onclick="viewStory('.$row['post_id'].')">
 							<h4><b>Event Date: '.date("M d, Y",strtotime($row['eventdate'])).'</b></h4>						
 							<h3 class="title">'.$row['title'].'</h3>	
-							<img src="'.$row['imgbanner'].'" style ="width:150px; height:150px; border-radius:50%" alt = "Avatar">		
+							<img src="'.$row['imgbanner'].'" alt = "Avatar">		
   							<p>'.decode($newstring).'</p>
 						</div>
 						<hr>
