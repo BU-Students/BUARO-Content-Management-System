6738f67d76727cad37b517afd23b2f2e751c67cd
cross_noire <alejandrejames.papina@bicol-u.edu.ph>
2017-10-01 19:46:09 +0800

    Missing Features Fixes
    -Some of the features were missing
    -This commit fixes them

--- admin/php/backend/update_content.php
+++ admin/php/backend/update_content.php
@@ -2,10 +2,10 @@
 		include 'connection.php';
 		$id = $_POST['id'];
 		if($_POST['content-type']==2){
-			if(isset($_FILES['img-banner']['tmp_name'])){
+			if(isset($_FILES['img-bannerev']['tmp_name'])){
 				$imgbannername = rand(903,3484798);
 				$imgbannerpath = "../../data/events-stories/".$imgbannername;
-				move_uploaded_file($_FILES['img-banner']['tmp_name'],'../../../data/events-stories/'.$imgbannername);
+				move_uploaded_file($_FILES['img-bannerev']['tmp_name'],'../../../data/events-stories/'.$imgbannername);
 				unlink("../".$_POST['curr-banner']);
 				$sql = "UPDATE post SET title = '".$_POST['post-title']."', post_type = ".$_POST['content-type'].", content = '".$_POST['content2']."', eventdate = '".$_POST['event-date']."', imgbanner = '".$imgbannerpath."' WHERE post_id = ".$id;
 			}
@@ -25,6 +25,5 @@
 		}
 
 		$conn->query($sql);
-		echo $_POST['curr-banner'];
 		header("Location:../eventstory.php");
 ?>
